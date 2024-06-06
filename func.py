82e28c072dbac9b8f047114e6cb7bd57da7d7166
Alexis Serrano <139297850+Alexis-Srrn@users.noreply.github.com>
2024-06-05 18:42:15 -0600

    Update func.py

--- func.py
+++ func.py
@@ -1,4 +1,4 @@
-mensaje = "Esto es un mensaje que acabo de cambiar"
+mensaje = "Esto es un mensaje que acabo de cambiar... y ahora otra vez para probar git fetch"
 mensaje_2 = "Antes de bajar del repo esto era una función de suma"
 
 mensaje_final = mensaje + mensaje_2
